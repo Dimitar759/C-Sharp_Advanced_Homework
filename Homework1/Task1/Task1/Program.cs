09eaa001a7619195bff161c6100a7630b6d62c4e
Dimitar Nikolov <dimitarnikolov759@gmail.com>
2024-03-28 21:03:40 +0100

    Task 1 done

---
+++ Homework1/Task1/Task1/Program.cs
@@ -0,0 +1,16 @@
+﻿using ClassLibrary1;
+using System.Reflection.Metadata;
+
+namespace Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            WebPage page = new WebPage();
+            Document doc = new Document();
+            ISearchable src = new WebPage();
+        }
+    }
+}
